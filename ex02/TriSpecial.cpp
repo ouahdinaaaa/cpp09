d7396ad88a86c6c366e9dd297b3df2018d76f744
ouahdinaaaa <a.ouahdina@gmail.com>
2023-12-31 19:16:45 +0100

    push

--- ex02/TriSpecial.cpp
+++ ex02/TriSpecial.cpp
@@ -6,7 +6,7 @@
 /*   By: ayael-ou <ayael-ou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/31 12:05:18 by ayael-ou          #+#    #+#             */
-/*   Updated: 2023/12/31 16:14:32 by ayael-ou         ###   ########.fr       */
+/*   Updated: 2023/12/31 19:16:13 by ayael-ou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 #include <iostream>
 #include <algorithm> 
 
-std::vector<int> TriSpecial(std::vector<int>& X) {
+std::vector<int> TriSpecial(std::vector<int>& X) 
+{
     int n = X.size();
 
     std::vector<int> ElementsPlusGrand;
     std::vector<std::pair<int, int>> pairs;
-    if (n == 1) {
+    if (n == 1) 
         return X;
-    }
 
     int m = n / 2;
 
@@ -33,30 +33,34 @@ std::vector<int> TriSpecial(std::vector<int>& X) {
         std::cout << "\npairs  i : first "  << pairs[i].first  << " ||| pairs  i : second : " << pairs[i].second  << std::endl;
     }
     
-    
     for (std::pair<int, int>& pair : pairs) {
-        if (pair.first < pair.second) {
+        if (pair.first < pair.second) 
+        {
             ElementsPlusGrand.push_back(pair.first);
         } else {
             ElementsPlusGrand.push_back(pair.second);
         }
     }
-
     ElementsPlusGrand = TriSpecial(ElementsPlusGrand);
+    for (std::vector<int>::iterator it = ElementsPlusGrand.begin(); it !=  count; i++)
+    {
+        /* code */
+    }
+    
+
 
     std::vector<int> resultat;
-
-    for (int i = 0; i < m; ++i) {
+    for (int i = 0; i < m; i++) {
         std::cout << "\npair i : " << pairs[i].first;
+        std::cout << "\npair i : " << pairs[i].second;
         resultat.push_back(pairs[i].first);
     }
 
     // Ajouter les éléments d'ElementsPlusGrand à la bonne position dans resultat
-    for (int i = 0; i < (int)ElementsPlusGrand.size(); ++i) {
-        int idx = std::upper_bound(resultat.begin(), resultat.end(), ElementsPlusGrand[i]) - resultat.begin();
-        resultat.insert(resultat.begin() + idx, ElementsPlusGrand[i]);
+    for (std::vector<int>::iterator it = ElementsPlusGrand.begin(); it != ElementsPlusGrand.end(); ++it) {
+        std::vector<int>::iterator idx = std::lower_bound(resultat.begin(), resultat.end(), *it);
+        resultat.insert(idx, *it);
     }
-
     return resultat;
 }
 
@@ -65,13 +69,12 @@ int main() {
     std::vector<int> Desorder = {3, 1, 4, 2, 5, 6,};
 
     std::vector<int> Tried = TriSpecial(Desorder);
-
     // Affichage des éléments triés
-    std::cout << "Liste triée : ";
-    for (std::vector<int>::iterator it = Tried.begin(); it != Tried.end(); ++it) {
+    std::cout << "\nListe triée : ";
+    for (std::vector<int>::iterator it = Tried.begin(); it != Tried.end(); ++it)
+    {
         std::cout << *it << " ";
     }
     std::cout << std::endl;
-
     return 0;
 }
